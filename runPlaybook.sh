f9807326ca4d73fc14957e39c58699be56d0ecd5
adelgado <adelgado0723@gmail.com>
2022-09-26 01:50:13 -0400

    fixing lsp installation issues

--- runPlaybook.sh
+++ runPlaybook.sh
@@ -1,3 +1,4 @@
-
-ansible-playbook $TAGS --ask-vault-pass --extra-vars "ansible_sudo_pass=test" local.yml
+TAGS='--tags="lsp"'
+# ansible-playbook $TAGS --ask-vault-pass --extra-vars "ansible_sudo_pass=test" local.yml
+ansible-playbook $TAGS  --extra-vars "ansible_sudo_pass=test" local.yml
 
